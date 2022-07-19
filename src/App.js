bbdbd338db4852ef7c1dd32701fdd999e8964691
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2022-07-19 22:44:13 +0530

    comparison between time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [time, setTime] = useState('')
+
+  const [output, setOutput] = useState(false)
+
+  const [show, setShow] = useState(false)
+
+  const handleTime = (e) => {
+    e.preventDefault();
+    var selectedTime = time;
+    var currentTime = new Date();
+    // console.log("Today Current Time", currentTime)
+    var selectedTimeStamp = (currentTime.getMonth() + 1) + "/" + currentTime.getDate() + "/" + currentTime.getFullYear() + " " + selectedTime;
+    var uservalue = new Date(selectedTimeStamp);
+    // console.log("user Input", uservalue, "Current value", currentTime)
+    if (uservalue < currentTime) {
+      setOutput(true)
+      setShow(true)
+    }
+    else {
+      setOutput(false)
+      setShow(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Main_container'>
+      <form onSubmit={(e) => handleTime(e)}>
+        <input type="text" value={time} onChange={(e) => setTime(e.target.value)} placeholder="Please Enter Time, for eg: 11:00 am" />
+        {
+          !time ? null : <button className='btn_submit' type='submit'>Check</button>
+        }
+      </form>
+      {
+        show ? <div className='output'>
+          {
+            output ? <p className='danger'>The Time Is Passed Away!</p> : output === false ? <p className='success'>The Time is Currently Running (Greater Than CurrentTime)...</p> : null
+          }
+        </div> : null
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
